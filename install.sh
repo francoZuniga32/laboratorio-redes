a1439c0899d6a6466133c70b87512bef035956ee
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2022-05-16 18:14:51 -0300

    feat: conectar dos redes mediante un router. configuracion de la tabla de ruteo

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/bash 
+
+sudo apt-get install xterm xwit telnetd wireshark lib32z1 libc6-i386 lib32readline-dev
+
+cd /usr/local/ 
+
+sudo wget http://mobiquo.gsyc.es/netgui/netgui-0.4.10.tar.bz2
+
+sudo tar -xvjSf netgui-0.4.10.tar.bz2
